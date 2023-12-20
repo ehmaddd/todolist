581935e363b0162ffea4b1db7eaf1d5553dd92fc
ehmaddd <ehmaddd@gmail.com>
2023-12-20 18:20:31 +0500

    Add basic structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,17 @@
+import { useState } from "react"
+
 export default function Home() {
+    const [tasks, setTasks] = useState([
+      'Go to walk', 'Have breakfast'
+    ]);
   return (
     <main>
       <h1>Main Page</h1>
+      <ul>
+      {tasks.map((task)=> {
+        <li>task</li>
+      })}
+      </ul>
     </main>
   )
 }
