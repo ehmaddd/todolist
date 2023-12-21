eeca440a82e6ba5e7e75643233e4c0517bbab157
ehmaddd <ehmaddd@gmail.com>
2023-12-21 23:47:12 +0500

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,8 @@
-/** @jsxImportSource @emotion/react */
-import { useState } from 'react';
-
 function MyPage() {
-  // Define state using the useState hook
-  const [count, setCount] = useState(0);
-
-  // Event handler to update the state
-  const handleIncrement = () => {
-    setCount(count + 1);
-  };
 
   return (
     <div>
       <h1>Main Page</h1>
-      <p>Count: {count}</p>
-      <button onClick={handleIncrement}>Increment</button>
     </div>
   );
 }
