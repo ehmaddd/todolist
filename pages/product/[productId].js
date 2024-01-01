f10cf951db79d46ad8283ffaffb5f3d234621e4e
ehmaddd <ehmaddd@gmail.com>
2024-01-01 17:27:12 +0500

    Get productId

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -2,9 +2,11 @@ import { useRouter } from "next/router";
 
 const productDetails = () => {
   const router = useRouter();
-    return (
-      <h2>Product Details</h2>
-    )
+  const productId = router.query.productId;
+
+  return (
+    <h2>Product Details</h2>
+  )
 }
 
 export default productDetails;
