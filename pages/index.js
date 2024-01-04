68c70586344cb6f08600d3e3558ea426a6db2cf6
ehmaddd <ehmaddd@gmail.com>
2024-01-04 19:49:33 +0500

    Update blog link in index

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,15 @@
-// pages/index.js
+import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
-// import TodoList from '../components/TodoList';
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
 
-  useEffect(() => {
-    // Load todos from local storage on mount
-    const storedTodos = JSON.parse(localStorage.getItem('todos')) || [];
-    setTodos(storedTodos);
-  }, []);
-
-  useEffect(() => {
-    // Save todos to local storage whenever the todos state changes
-    localStorage.setItem('todos', JSON.stringify(todos));
-  }, [todos]);
-
-  const handleToggle = (index) => {
-    const newTodos = [...todos];
-    newTodos[index].completed = !newTodos[index].completed;
-    setTodos(newTodos);
-  };
-
   return (
     <div>
       <h1>Todo List</h1>
-      {/* <TodoList todos={todos} onToggle={handleToggle} /> */}
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
     </div>
   );
 };
