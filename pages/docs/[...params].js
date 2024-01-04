81202a2c8acfd6b6c632a7804790812fe3eb44e5
ehmaddd <ehmaddd@gmail.com>
2024-01-04 10:15:29 +0500

    Extract params from router query

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -2,8 +2,11 @@ import { useRouter } from "next/router";
 
 const Doc = () => {
   const router = useRouter();
+  const { params } = router.query;
+
   return (
     <h1>Doc Home Page</h1>
+
   )
 }
 
