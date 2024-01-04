8ef64487ce04c0fe0c9e8acc666e425cf4fea245
ehmaddd <ehmaddd@gmail.com>
2024-01-04 10:25:51 +0500

    Conditional rendering

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -5,13 +5,20 @@ const Doc = () => {
   const { params = [] } = router.query;
 
   if(params.length === 2){
-
+    return (
+      <h1>Viewing docs for feature {params[0]} and concept {params[1]}</h1>
+    )
   }
-
-  return (
-    <h1>Doc Home Page</h1>
-
-  )
+  else if(params.length === 1){
+    return (
+      <h1>Viewing docs for feature {params[0]}</h1>
+    )
+  }
+  else {
+    return (
+      <h1>Doc Home Page</h1>
+    )
+  }
 }
 
   export default Doc;
