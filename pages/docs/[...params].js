bb576b06f1f411b301c0227ef0052377b3664168
ehmaddd <ehmaddd@gmail.com>
2024-01-04 10:17:10 +0500

    Console params

--- pages/docs/[...params].js
+++ pages/docs/[...params].js
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 const Doc = () => {
   const router = useRouter();
   const { params } = router.query;
+  console.log(params);
 
   return (
     <h1>Doc Home Page</h1>
