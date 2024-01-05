00255c0b22a88d49f14fa0c3a197c0edc0a0e7cb
ehmaddd <ehmaddd@gmail.com>
2024-01-05 19:32:07 +0500

    Link to home

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,6 +1,11 @@
+import Link from "next/link";
+
 const product = () => {
     return (
       <>
+        <Link href="/">
+          Home
+        </Link>
         <h1>PRODUCTS</h1>
         <ul>
           <li>Product 1</li>
