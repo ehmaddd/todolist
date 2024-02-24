72c5ea9e62617d5f3ddd388aacd62539dead6f8c
ehmaddd <ehmaddd@gmail.com>
2024-02-24 22:48:15 +0500

    Add style in index

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 // pages/index.js
 import { useState } from 'react';
 import TodoList from '../components/TodoList';
+import styles from '../styles/Home.module.css';
 
 const Home = () => {
   const [tasks, setTasks] = useState([]);
@@ -22,15 +23,20 @@ const Home = () => {
   };
 
   return (
-    <div>
-      <h1>To-Do List</h1>
-      <input
-        type="text"
-        value={newTaskText}
-        onChange={(e) => setNewTaskText(e.target.value)}
-        placeholder="Add new task"
-      />
-      <button onClick={addTask}>Add</button>
+    <div className={styles.container}>
+      <h1 className={styles.title}>To-Do List</h1>
+      <div className={styles.inputContainer}>
+        <input
+          type="text"
+          value={newTaskText}
+          onChange={(e) => setNewTaskText(e.target.value)}
+          placeholder="Add new task"
+          className={styles.input}
+        />
+        <button onClick={addTask} className={styles.addButton}>
+          Add
+        </button>
+      </div>
       <TodoList tasks={tasks} onDelete={deleteTask} onUpdate={updateTask} />
     </div>
   );
