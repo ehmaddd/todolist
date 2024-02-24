3e94a2b8bb5f6cf8f77f116be7fa413f90351cea
ehmaddd <ehmaddd@gmail.com>
2024-02-24 22:45:40 +0500

    Add style in Todolist

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,9 +1,10 @@
 // components/TodoList.js
 import Task from './Task';
+import styles from '../styles/TodoList.module.css';
 
 const TodoList = ({ tasks, onDelete, onUpdate }) => {
   return (
-    <div>
+    <div className={styles.todoList}>
       {tasks.map((task) => (
         <Task key={task.id} task={task} onDelete={onDelete} onUpdate={onUpdate} />
       ))}
