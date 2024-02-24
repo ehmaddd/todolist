980be23a04ce4d7427aaef40570526144becb36f
ehmaddd <ehmaddd@gmail.com>
2024-02-24 22:38:57 +0500

    Populate index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,37 @@
-import Link from 'next/link';
-import React, { useState, useEffect } from 'react';
+// pages/index.js
+import { useState } from 'react';
+import TodoList from '../components/TodoList';
 
 const Home = () => {
-  const [todos, setTodos] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [newTaskText, setNewTaskText] = useState('');
+
+  const addTask = () => {
+    if (newTaskText.trim() !== '') {
+      setTasks([...tasks, { id: Date.now(), text: newTaskText }]);
+      setNewTaskText('');
+    }
+  };
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  const updateTask = (id, newText) => {
+    setTasks(tasks.map((task) => (task.id === id ? { ...task, text: newText } : task)));
+  };
 
   return (
     <div>
-      <h1>Todo List</h1>
-      <Link href="/blog">
-        Blog
-      </Link>
-      <Link href="/product">
-        Product
-      </Link>
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={newTaskText}
+        onChange={(e) => setNewTaskText(e.target.value)}
+        placeholder="Add new task"
+      />
+      <button onClick={addTask}>Add</button>
+      <TodoList tasks={tasks} onDelete={deleteTask} onUpdate={updateTask} />
     </div>
   );
 };
