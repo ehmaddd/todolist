c9e007bd8a0ca96347acfc8c4bf87dde422a8725
ehmaddd <ehmaddd@gmail.com>
2024-02-24 22:36:59 +0500

    Update Task.js file

--- components/Task.js
+++ components/Task.js
@@ -1,12 +1,24 @@
-import React from 'react';
+// components/Task.js
+import { useState } from 'react';
 
-const Task = ({ task, onDelete }) => {
-    return (
-      <div>
-        <span>{task.text}</span>
-        <button onClick={() => onDelete(task.id)}>Delete</button>
-      </div>
-    );
+const Task = ({ task, onDelete, onUpdate }) => {
+  const [text, setText] = useState(task.text);
+
+  const handleDelete = () => {
+    onDelete(task.id);
+  };
+
+  const handleUpdate = () => {
+    onUpdate(task.id, text);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+      <button onClick={handleUpdate}>Update</button>
+      <button onClick={handleDelete}>Delete</button>
+    </div>
+  );
 };
 
 export default Task;
