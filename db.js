8602cbf94bf49800a55c581ba512396d73f84a98
ehmaddd <ehmaddd@gmail.com>
2024-03-28 21:40:32 +0500

    Update db config

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-
+    user: 'postgres',
+    host: 'localhost',
+    database: 'todo',
+    password: '1234.com',
+    port: 5432,
 });
 
 module.exports = pool;
