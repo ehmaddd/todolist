dea1413dc7075ec894fa46ad71e2a6327c65cc05
ehmaddd <ehmaddd@gmail.com>
2024-04-13 22:11:15 +0500

    Server port

--- app.js
+++ app.js
@@ -8,5 +8,5 @@ app.use('/api/auth', require('./routes/auth'));
 app.use('/api/tasks', require('./routes/tasks'));
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running : port ${port}`);
 });
